d7962508ff72456e488e524f08e0a07bfb51f2a8
spencerwooo <spencerwoo98@foxmail.com>
2019-03-27 11:58:19 +0800

    Update: Add comments for consumer process

--- Windows/consumer.cpp
+++ Windows/consumer.cpp
@@ -10,24 +10,26 @@ using namespace std;
 // 生成一个 1~3 秒的随机时间
 int getRandomDelay()
 {
-  srand(time(NULL));
+  srand(time(NULL) + GetCurrentProcessId());
   return rand() % 3 + 1;
 }
 
 int main(int argc, char const *argv[])
 {
-  cout << "[CONSUMER] Consumer ID: " << argv[1]
-       << ", PID: " << GetCurrentProcessId() << endl;
-
+  // 共享主存的句柄变量
   HANDLE conSharedMappingFileHandle;
+  // 文件映射视图变量（指针）
   LPVOID conShmFilePointer;
 
+  // 读取共享映射文件
   conSharedMappingFileHandle = OpenFileMapping(FILE_MAP_ALL_ACCESS, 0, SHM_NAME);
   if (conSharedMappingFileHandle == NULL)
   {
     cerr << "[ERR] Consumer could not open file mapping object: " << GetLastError() << endl;
     return 1;
   }
+
+  // 读取文件映射上的视图
   conShmFilePointer = MapViewOfFile(conSharedMappingFileHandle, FILE_MAP_ALL_ACCESS, 0, 0, 0);
   if (conShmFilePointer == NULL)
   {
@@ -36,27 +38,39 @@ int main(int argc, char const *argv[])
     return 1;
   }
 
+  // 获取指向共享缓冲区指针
   struct sharedMemory *pcpSharedMemory = (struct sharedMemory *)conShmFilePointer;
+  // 获取三个信号量的句柄
   HANDLE producerSemEmpty = OpenSemaphore(SEMAPHORE_ALL_ACCESS, FALSE, SEM_EMPTY);
   HANDLE producerSemFull = OpenSemaphore(SEMAPHORE_ALL_ACCESS, FALSE, SEM_FULL);
   HANDLE producerMutex = OpenMutex(MUTEX_ALL_ACCESS, FALSE, SEM_MUTEX);
 
+  // 生产者重复生产
   for (int i = 0; i < CONSUME_CYCLES; i++)
   {
+    // 两个 P 操作，先申请 FULL，再申请 MUTEX
     WaitForSingleObject(producerSemFull, INFINITE);
     WaitForSingleObject(producerMutex, INFINITE);
+
+    // 随机一个等待时间
     Sleep(getRandomDelay() * 1000);
 
+    // 取出 buffer 头部物品
     char stock = pcpSharedMemory->data.buffer[pcpSharedMemory->data.head];
+    // 将头部指针 head 向前移动一位
     pcpSharedMemory->data.head = (pcpSharedMemory->data.head + 1) % BUFFER_SIZE;
 
+    // 判断 buffer 是否为空
     if (pcpSharedMemory->data.head == pcpSharedMemory->data.tail)
     {
       pcpSharedMemory->data.isEmpty = TRUE;
     }
 
-    cout << "[CONSUMER] Consumer " << i << " consumes: " << stock << " | BUFFER: ";
+    // 输出本次操作内容
+    cout << "[CONSUMER] Consumer " << i + 4 << " consumes: " << stock << " | BUFFER: ";
 
+
+    // 输出 buffer 内容
     if (pcpSharedMemory->data.isEmpty == TRUE)
     {
       cout << "-";
@@ -71,12 +85,16 @@ int main(int argc, char const *argv[])
 
     cout << endl;
 
+
+    // 两个 V 操作，先释放 MUTEX，再释放 EMPTY
     ReleaseMutex(producerMutex);
     ReleaseSemaphore(producerSemEmpty, 1, NULL);
   }
 
+  // 关闭文件映射视图
   UnmapViewOfFile(conShmFilePointer);
   conShmFilePointer = NULL;
+  // 关闭共享主存句柄
   CloseHandle(conSharedMappingFileHandle);
 
   return 0;
