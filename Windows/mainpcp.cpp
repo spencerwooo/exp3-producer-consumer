2eb164138b20f192adce70f5083ad2318352ab2c
spencerwooo <spencerwoo98@foxmail.com>
2019-03-27 11:51:37 +0800

    Update: Add comments information for main process cpp

--- Windows/mainpcp.cpp
+++ Windows/mainpcp.cpp
@@ -7,52 +7,77 @@
 
 using namespace std;
 
+// 生产者、消费者可执行文件
 TCHAR PRODUCER_EXE_PATH[] = TEXT("producer.exe");
 TCHAR CONSUMER_EXE_PATH[] = TEXT("consumer.exe");
 
+// 生产者消费者子进程、线程的句柄集合
 HANDLE createdProcessHandles[TOTAL_PROCESS_COUNT + 1];
 HANDLE createdThreadHandles[TOTAL_PROCESS_COUNT + 1];
 
+// 创建进程
 void ProcessCreation(int processId)
 {
+  // 进程的启动信息结构体
   STARTUPINFO si;
+  // 进程的创建信息结构体
   PROCESS_INFORMATION pi;
+  // 初始化各个结构体
   ZeroMemory(&si, sizeof(si));
   si.cb = sizeof(si);
   ZeroMemory(&pi, sizeof(pi));
 
+  // 创建子进程，我们根据 processId 来区分创建生产者还是消费者
+  // 如果 processId 位于 1~3 之间则创建生产者 producer
   if (processId >= PRODUCER_PROCESS_ID_HEAD && processId <= PRODUCER_PROCESS_ID_TAIL)
   {
-    TCHAR cmdLine[MAX_PATH];
-    _stprintf(cmdLine, "%s %d", PRODUCER_EXE_PATH, processId);
-    BOOL processCreationOk = CreateProcess(PRODUCER_EXE_PATH, cmdLine, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
+    // TCHAR cmdLine[MAX_PATH];
+    // _stprintf(cmdLine, "%s %d", PRODUCER_EXE_PATH, processId);
+
+    // 获得创建进程返回值，对异常进行处理
+    BOOL processCreationOk = CreateProcess(PRODUCER_EXE_PATH, NULL, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
     if (!processCreationOk)
     {
       cerr << "[ERR] Process creation failed with " << GetLastError() << "." << endl;
       return;
     }
+
+    // 输出创建的进程信息
+    cout << "[CREATED] Producer ID: " << processId
+         << ", PID: " << GetCurrentProcessId() + processId << endl;
   }
+  // 如果 processId 位于 4~7 之间则创建消费者 consumer
   else if (processId >= CONSUMER_PROCESS_ID_HEAD && processId <= CONSUMER_PROCESS_ID_TAIL)
   {
-    TCHAR cmdLine[MAX_PATH];
-    _stprintf(cmdLine, "%s %d", CONSUMER_EXE_PATH, processId);
-    BOOL processCreationOk = CreateProcess(CONSUMER_EXE_PATH, cmdLine, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
+    // TCHAR cmdLine[MAX_PATH];
+    // _stprintf(cmdLine, "%s %d", CONSUMER_EXE_PATH, processId);
+
+    // 获得创建进程返回值，对异常进行处理
+    BOOL processCreationOk = CreateProcess(CONSUMER_EXE_PATH, NULL, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
     if (!processCreationOk)
     {
       cerr << "[ERR] Process creation failed with " << GetLastError() << "." << endl;
       return;
     }
+
+    // 输出创建的进程信息
+    cout << "[CREATED] Consumer ID: " << processId
+         << ", PID: " << GetCurrentProcessId() + processId << endl;
   }
 
+  // 将创建的进程和线程返回的句柄存入集合，方便主进程监控
   createdProcessHandles[processId] = pi.hProcess;
   createdThreadHandles[processId] = pi.hThread;
 }
 
 int main(int argc, char const *argv[])
 {
+  // 主进程开始
   cout << "[MAIN] Main process start." << endl;
 
+  // 共享主存的句柄变量
   HANDLE sharedMappingFileHandle;
+  // 文件映射视图变量（指针）
   LPVOID shmFilePointer;
 
   // 创建共享映射文件句柄
@@ -72,6 +97,7 @@ int main(int argc, char const *argv[])
     return 1;
   }
 
+  // 创建共享缓存区结构体指针
   struct sharedMemory *pcpSharedMemory = (struct sharedMemory *)shmFilePointer;
 
   // 初始化共享缓存区
@@ -96,10 +122,14 @@ int main(int argc, char const *argv[])
   while (nextProcessId <= TOTAL_PROCESS_COUNT)
   {
     ProcessCreation(nextProcessId++);
+    Sleep(500);
   }
+  // 主进程依次等待 7 个子进程返回
   for (int i = 1; i <= TOTAL_PROCESS_COUNT; i++)
   {
+    // 等待子进程返回
     WaitForSingleObject(createdProcessHandles[i], INFINITE);
+    // 关闭进程和线程的句柄
     CloseHandle(createdProcessHandles[i]);
     CloseHandle(createdThreadHandles[i]);
   }
