a4e3f253e850561f8e3169ed641bc2c7b63b0632
spencerwooo <spencerwoo98@foxmail.com>
2019-03-27 11:52:40 +0800

    Update: Add header file comments

--- Windows/mainpcp.h
+++ Windows/mainpcp.h
@@ -1,10 +1,11 @@
 #include <tchar.h>
 #include <windows.h>
+
+// 共享主存大小 1024 字节
 const int SHARED_MEM_SIZE = 1024;
 
 // 缓冲区大小为 4
 const int BUFFER_SIZE = 4;
-// 3 个生产者重复 4 次，4 个消费者重复 3 次
 // 三个生产者进程 ID = 1、2、3
 const int PRODUCER_PROCESS_ID_HEAD = 1;
 const int PRODUCER_PROCESS_ID_TAIL = 3;
